c52be554fd246850e0bca5e71a75c48efd585017
Wouter Hof <wouterhof@MacBook-Pro-van-Wouter.local>
2026-08-19 09:54:03 +0200

    meta tag toegevoegd 19 aug

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE HTML>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./styles.css">
     <title>'t Zwolse kunsthuis</title>
 </head>
